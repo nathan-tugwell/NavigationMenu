314bd40fe0b6cccbe99c8cafb2c5ff8d30604d4d
Nathan <nathan.tugwell@me.com>
2018-11-14 15:28:59 +0000

    json parsing completed and updated UI

--- NavigationMenu/Controllers/HomePageViewController.swift
+++ NavigationMenu/Controllers/HomePageViewController.swift
@@ -7,47 +7,16 @@
 //
 
 import UIKit
-import Alamofire
-
-typealias JSON = [String: Any]
-
-
-struct NavJSON: Codable {
-    let nav: Nav
-}
-struct Nav: Codable {
-        let nav: [JSON]
-}
-
 
 class HomePageViewController: UIViewController {
     
-    var navs = [Nav]()
-    
-    let API_URL = "https://s3-eu-west-1.amazonaws.com/api.themeshplatform.com/nav.json"
-
     @IBOutlet weak var menuButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        menuButton.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
-        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
-        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
-        
-            Alamofire.request(API_URL).responseJSON { response in
-                let json = response.data
-                
-                do{
-                    let decoder = JSONDecoder()
-                    self.navs = try decoder.decode([Nav].self, from: json!)
-                    
-//                    for nav in self.navs {
-//                        print(nav.navHome)
-//                    }
-                } catch let error {
-                    print(error)
-                }
-            }
-        }
-
+                menuButton.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+                self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+                self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+    
 }
--- NavigationMenu/Controllers/MenuViewController.swift
+++ NavigationMenu/Controllers/MenuViewController.swift
@@ -7,32 +7,79 @@
 //
 
 import UIKit
-import Alamofire
+
+struct Menu: Decodable {
+    var nav = [Menus]()
+    
+}
+
+struct Menus: Decodable {
+    var name: String
+}
+
 
 class MenuViewController: UIViewController {
     
-    var navs = [Nav]()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var menu: Menu?
     
     let API_URL = "https://s3-eu-west-1.amazonaws.com/api.themeshplatform.com/nav.json"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        Alamofire.request(API_URL).responseJSON { response in
-            let json = response.data
-            
-            do{
+        guard let url = URL(string: API_URL) else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else { return }
+            do {
                 let decoder = JSONDecoder()
+                self.menu = try decoder.decode(Menu.self, from: data)
+//                print(self.menu?.nav as Any)
                 
-                self.navs = try decoder.decode([Nav].self, from: json!)
-                
-                for nav in self.navs {
-                    print(nav.navHome)
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
                 }
+                
             } catch let error {
-                print(error)
+                print("failed", error)
             }
-        }
+            }.resume()
+        
     }
 
+}
+
+extension MenuViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.menu?.nav.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        
+        if cell == nil {
+            cell = UITableViewCell.init(style: .value1, reuseIdentifier: "cell")
+        }
+        let navItem = self.menu?.nav[indexPath.row]
+        cell?.textLabel?.text = navItem?.name
+        cell?.accessoryView = UIImageView(image: UIImage(named: "icons8-chevron-right-50"))
+        
+        return cell!
+    }
+    
+    
+}
+
+extension MenuViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0.1
+    }
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0.1
+    }
 }
